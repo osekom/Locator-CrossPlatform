f0d10f7beaa14278ac978072361393e4d6b6e332
4jay <leonardomanrique9@gmail.com>
2017-09-13 10:42:27 -0500

    Add!
    Files Project Add

---
+++ LocatorXamarin/LocatorXamarin/LocatorXamarin/AppConst/Constantes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocatorXamarin.AppConst
+{
+    public class Constantes
+    {
+        // establecer el Link URL de tu seridor Locator
+        public static string URL = "";
+    }
+}
---
+++ LocatorXamarin/LocatorXamarin/LocatorXamarin/MainPageLocator.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LocatorXamarin
+{
+    public partial class MainPageLocator : ContentPage
+    {
+        public string URL = AppConst.Constantes.URL;
+
+        public MainPageLocator()
+        {
+            InitializeComponent();
+            Browser.Source = URL;
+        }
+    }
+}
